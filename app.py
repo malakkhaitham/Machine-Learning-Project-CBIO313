0b6cedc96c63567848b1a5ada6e12fd18eba4ea1
Malak Haitham <m.haitham2296@nu.edu.eg>
2025-06-06 17:58:03 +0300

    Finalize requirements and prepare for deployment

--- app.py
+++ app.py
@@ -32,5 +32,7 @@ def predict():
     except Exception as e:
         return render_template('index.html', prediction_text=f'Error: {str(e)}')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
